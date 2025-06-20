d0a4d99f63c9b8fcc03c4fce5e451f496e79701d
Gowtham <gowtham.s@theporter.in>
2025-06-20 19:58:49 +0530

    products listed on products page.

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
+
 const ProductsPage = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        try {
+            const response = await fetch('https://fakestoreapi.com/products');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setProducts(data);
+        } catch (error) {
+            console.error('Failed to fetch products:', error);
+        }
+    }
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
     return (
         <>
             <p>Products Page</p>
+            {products.map((product) => (
+                <div key={product.id}>
+                    <h3>Product: {product.title}</h3>
+                    <p>Price: ${product.price}</p>
+                    <p>Description: {product.description}</p>
+                </div>
+            ))}
         </>
     )
 }
